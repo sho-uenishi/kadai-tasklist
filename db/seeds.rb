8517d817b4deca50447c08270ee562664bbee815
sho.uenishi <standing-too-far@docomo.ne.jp>
2020-05-10 03:35:56 +0000

    kadai-tasklist

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Task.create(status: 'test 1', content: 'test content 1')
+Task.create(status: 'test 1', content: 'test 1')
