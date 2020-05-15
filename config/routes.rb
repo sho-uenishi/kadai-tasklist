0de07311c5a3a741e45d699341617a118edecae5
sho.uenishi <standing-too-far@docomo.ne.jp>
2020-05-15 10:04:54 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   
@@ -11,4 +8,6 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :tasklist, only: [:create, :destroy]
 end
