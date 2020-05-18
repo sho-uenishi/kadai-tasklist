acbd6103a4d43f81ad006c446f8ca03a360d602b
sho.uenishi <standing-too-far@docomo.ne.jp>
2020-05-18 06:23:23 +0000

    root

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to root_url
+      root_to @user
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
