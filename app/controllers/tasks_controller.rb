5ac4a445bbc22942b3aab9bbb4262e4d72557319
sho.uenishi <standing-too-far@docomo.ne.jp>
2020-05-19 12:06:27 +0000

    tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,7 +17,7 @@ class TasksController < ApplicationController
     @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'メッセージを投稿しました。'
-      redirect_to @task
+      redirect_to root_url
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
